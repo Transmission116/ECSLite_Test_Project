e4111daf51858c7ae5396477f2a4fa30d111fb14
HELL\Kot <pavel-akl@yandex.ru>
2022-07-08 18:01:09 +0300

    # Add Mono Systems  - LevelMonoSpawnSystem  - MoveViewSystem  - PlayermonoInit

---
+++ Assets/Spripts/Systems/MonoSystems/LevelMonoSpawnSystem.cs
@@ -0,0 +1,83 @@
+﻿using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace ECS_Lite_Test
+{
+    internal sealed class LevelMonoSpawnSystem : IEcsInitSystem
+    {
+        private EcsPoolInject<TransformComponent> _transformPool = default;
+  
+
+        private EcsCustomInject<PrefabFactory> _prefabFactory;
+        private EcsCustomInject<AssetData> _assetData;
+        
+        
+        public void Init(EcsSystems systems)
+        {
+            SpawnButtons(systems);
+            SpawnDoors(systems);
+            SpawnLevel();
+        }
+
+        private void SpawnDoors(EcsSystems systems)
+        {
+            EcsWorld world = systems.GetWorld();
+
+            EcsFilter filter = world.Filter<PositionComponent>()
+                .Inc<LinkedIdComponent>()
+                .Inc<MoveSpeedComponent>()
+                .Inc<CanBeActiveLinkedComponent>()
+                .Exc<PushableComponent>()
+                .End();
+
+
+            EcsPool<PositionComponent> positionPool = world.GetPool<PositionComponent>();
+            EcsPool<LinkedIdComponent> linkedIdPool = world.GetPool<LinkedIdComponent>();
+            foreach (var entity in filter)
+            {
+                ref PositionComponent posComp = ref positionPool.Get(entity);
+                ref LinkedIdComponent linkComp = ref linkedIdPool.Get(entity);
+
+                VisualView doorObject = _prefabFactory.Value.CreatePrefab(_assetData.Value.DoorsPrefab,posComp.Value);
+                doorObject.SetupVisualView(linkComp.Value);
+                
+                ref TransformComponent transComp = ref _transformPool.Value.Add(entity);
+                transComp.Transform = doorObject.Transform;
+            }
+        }
+
+        private void SpawnButtons(EcsSystems systems)
+        {
+            EcsWorld world = systems.GetWorld();
+
+            EcsFilter filter = world.Filter<PositionComponent>()
+                .Inc<LinkedIdComponent>()
+                .Inc<DistanceCheckComponent>()
+                .Inc<PushableComponent>()
+                .Exc<CanBeActiveLinkedComponent>()
+                .End();
+
+
+            EcsPool<PositionComponent> positionPool = world.GetPool<PositionComponent>();
+            EcsPool<LinkedIdComponent> linkedIdPool = world.GetPool<LinkedIdComponent>();
+            foreach (var entity in filter)
+            {
+                ref PositionComponent posComp = ref positionPool.Get(entity);
+                ref LinkedIdComponent linkComp = ref linkedIdPool.Get(entity);
+                
+                VisualView buttonObject = _prefabFactory.Value.CreatePrefab(_assetData.Value.ButtonPrefab,posComp.Value);
+                buttonObject.SetupVisualView(linkComp.Value);
+                
+                ref TransformComponent transComp = ref _transformPool.Value.Add(entity);
+                transComp.Transform = buttonObject.Transform;
+            }
+        }
+
+        private void SpawnLevel()
+        {
+            int levelIndex = 0; //can be use for generate level from array 
+            _prefabFactory.Value.CreatePrefab(_assetData.Value.LevelPrefab[levelIndex], Vector3.zero);
+        }
+    }
+}
---
+++ Assets/Spripts/Systems/MonoSystems/PlayerMonoInitSystem.cs
@@ -0,0 +1,32 @@
+using System.IO.Enumeration;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace ECS_Lite_Test {
+    sealed class PlayerMonoSpawnSystem : IEcsInitSystem 
+    {
+        private EcsPoolInject<TransformComponent> _poolTransform = default;
+        private EcsPoolInject<AnimatorComponent> _poolAnimator = default;
+        
+        private EcsCustomInject<PrefabFactory> _prefabFactory;
+        private EcsCustomInject<AssetData> _assetData;
+        
+        public void Init (EcsSystems systems)
+        {
+            EcsWorld world = systems.GetWorld();
+            EcsFilter filter = world.Filter<ControlledByPlayer>()
+                .Inc<PositionComponent>()
+                .Inc<MoveSpeedComponent>().End();
+
+            foreach (var entity in filter)
+            {
+                EcsPool<PositionComponent> poolPosition = world.GetPool<PositionComponent>();
+                
+                VisualView playerUnit = _prefabFactory.Value.CreatePrefab(_assetData.Value.PlayerPrefab, poolPosition.Get(entity).Value);
+                _poolTransform.Value.Add(entity).Transform = playerUnit.Transform;
+                _poolAnimator.Value.Add(entity).Animator = playerUnit.GetComponent<Animator>();
+            }
+        }
+    }
+}
